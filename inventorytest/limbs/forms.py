cfd5fc7ba9fce20eed25343b002561f5d7918588
Kelvin Yip <kelvinyip7@Kelvins-MBP.wireless.yale.internal>
2020-10-25 17:28:45 -0400

    changes

--- inventorytest/limbs/forms.py
+++ inventorytest/limbs/forms.py
@@ -13,8 +13,19 @@ class ItemsForm(forms.ModelForm):
             'comment'
         ] 
         widgets = {
-            'name': forms.TextInput(attrs={'class':"form-control" , 'type':"text", 'placeholder':"name", 'width':"50%"}),
-            'supplier_id': forms.Select(attrs = {'class':"form-control"}),
+            'name': forms.TextInput(
+                attrs = {
+                    'class': "form-control", 
+                    'type': "text", 
+                    'placeholder': "Name", 
+                    'width': "50%"
+                    }
+                ),
+            'supplier_id': forms.Select(
+                attrs = {
+                    'class': "form-control"
+                    }
+                ),
             'part_number': forms.TextInput(attrs={'class':"form-control" , 'type':"text", 'placeholder':"partnumber", 'width':"50%"}),
             'url': forms.TextInput(attrs={'class':"form-control" , 'type':"text", 'placeholder':"url", 'width':"50%"}),
             'description': forms.TextInput(attrs={'class':"form-control" , 'type':"text", 'placeholder':"description", 'width':"50%"}),
