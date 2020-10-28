e4f92692b9ec0c1f79deb4fa6284e9ac11542457
Oliver Ye <olivermax700@yahoo.com>
2020-10-27 21:16:04 -0400

    stash

--- inventorytest/limbs/urls.py
+++ inventorytest/limbs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('itemforms', views.items_create_view, name = 'item_create_view'), 
     path ('modalforms', views.modal_create_view, name = 'modal_create_view'),
+    path('<id>/delete', views.delete_view, name = 'delete_view'), 
 ]
--- inventorytest/limbs/views.py
+++ inventorytest/limbs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import (get_object_or_404, 
+                              render,  
+                              HttpResponseRedirect) 
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
@@ -16,35 +17,81 @@ def index(request):
     return HttpResponse(template.render(context,request))
 
 def items_create_view(request): 
-    form = ItemsForm(request.POST)
-    template = loader.get_template('limbs/formtrial.html')
-    if form.is_valid():
-        form.save()
-        print("form was saved") 
-    else:
-        print("form was not validated")
-        form = ItemsForm()
-    context = {
-        'form': form
-    }
-    return HttpResponse(template.render(context,request))
+        form = ItemsForm(request.POST)
+        template = loader.get_template('limbs/formtrial.html')
+        if form.is_valid():
+            form.save()
+            print("form was saved") 
+        else:
+            print("form was not validated")
+            form = ItemsForm()
+        context = {
+            'form': form
+        }
+        return HttpResponse(template.render(context,request))
     
 def modal_create_view(request):
-    form = ItemsForm(request.POST)
+    form = ItemsForm()
     item_list = Items.objects.order_by('name')
-    print("item list one:\n")
-    print(item_list)
     template = loader.get_template('limbs/modaltrial.html')
+    print("gets here?")
     print(request.POST)
-    if form.is_valid():
-        t = form.save()
-    else:
-        form = ItemsForm()
-    print("item list two:\n")
-    print(item_list)
+    if 'add' in request.POST:   
+        form = ItemsForm(request.POST)
+        if form.is_valid():
+            t = form.save()
+        else:
+            form = ItemsForm()
+    elif 'update' in request.POST:
+        print("update")
+    elif 'delete' in request.POST:
+        obj = get_object_or_404(Items, name = request.POST.get("name")) 
+        obj.delete()
     context = {
         'form': form,
         'item_list': item_list 
     }
     return HttpResponse(template.render(context,request))
+
+
+def delete_view(request, id):
+    print("gets here from delete request")
+    if request.method == "DELETE": 
+        try:
+            item = Items.objects.get(name = id)
+        except Item.DoesNotExist: 
+            return redirect('/limbs')
+        item.delete()
+    redirect('/limbs')
+
+class MyFormViews: 
+
+    def get(self, request, *args, **kwargs):
+        form = ItemsForm(request.POST)
+        item_list = Items.objects.order_by('name')
+        template = loader.get_template('limbs/modaltrial.html')
+        return HttpResponse(template.render(context,request))
+
+        
+    def post(self, request, *args, **kwargs):
+        form = ItemsForm(request.POST)
+        if 'add' in request.POST:
+            print("gets in to the addition")
+            if form.is_valid():
+                t = form.save()
+            else:
+                form = ItemsForm()
+        elif 'update' in request.POST:
+            print("it should update")
+        elif 'delete' in request.POST:
+            print("it should delete it")
+        context = {}
+        return HttpResponse(template.render(context,request))
+
+    
+
+        
+
+
+        
 
