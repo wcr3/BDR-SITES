72c4b4b5d3fd9055e44adfd69e7e1541dddd66cf
Oliver Ye <olivermax700@yahoo.com>
2020-11-12 13:56:00 -0500

    basic search bar

--- inventorytest/limbs/views.py
+++ inventorytest/limbs/views.py
@@ -5,14 +5,18 @@ from django.http import HttpResponse
 from django.template import loader
 from .forms import ItemsForm
 from .models import Items
+from .filter import ItemsFilter
 
 def index(request):
     item_list = Items.objects.order_by('name')
     form = ItemsForm()
     template = loader.get_template('limbs/index.html')
+    item_filter = ItemsFilter(request.GET, queryset=item_list)
+    item_list = item_filter.qs
     context = {
         'item_list': item_list,
-        'form': form
+        'form': form,
+        'item_filter':item_filter
     }
     return HttpResponse(template.render(context,request))
 
@@ -31,9 +35,9 @@ def delete_view(request, id):
 
 def update_view(request, id):
     item = Items.objects.get(id=id)
-    form = ItemsForm(request.POST, instance = item) 
-    if form.is_valid(): 
-        form.save() 
-    else: 
+    form = ItemsForm(request.POST, instance = item)
+    if form.is_valid():
+        form.save()
+    else:
         print(form.errors)
     return redirect('/limbs')
