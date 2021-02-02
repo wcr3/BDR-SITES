25a36202e4694b1ca7dd237a1a2eea551bb2544d
Kelvin Yip <kelvinyip7@Kelvins-MBP.home>
2021-02-02 16:36:22 -0500

    suppliers and locations pages

--- limbs/urls.py
+++ limbs/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
 from django.views import generic
 
-from .models import Item
+from .models import Item, Supplier, Location
 
 from . import views
 
 app_name = 'limbs'
 urlpatterns = [
     path('', generic.ListView.as_view(model=Item, template_name='limbs/index.html'), name='index'),
-    path('item_table/<str:search>', views.item_table, name='item_table'),
+    path('supplier_list', generic.ListView.as_view(model=Supplier, template_name='limbs/suppliers.html'), name="supplier_table"),
+    path('location_list', generic.ListView.as_view(model=Location, template_name="limbs/locations.html"), name="location_table"),
+    # path('supplier_list', views.supplier_table, name='supplier_table'),
+    # path('location_list', views.location_table, name='location_table'),
+    # path('item_table/<str:search>', views.item_table, name='item_table'),
+    # path('supplier_table', views.supplier_table, name='supplier_table'),
     path('item_popup/<int:pk>', views.item_popup, name='item_popup'),
-    path('create_item', views.create_item, name='create_item'), 
-    path('edit_item/<int:pk>', views.edit_item, name='edit_item')
+    path('supplier_popup/<int:pk>', views.supplier_popup, name='supplier_popup'),
+    path('location_popup/<int:pk>', views.location_popup, name='location_popup'),
+    path('create_item', views.create_item, name='create_item'),
+    path('create_supplier', views.create_supplier, name='create_supplier'),
+    path('create_location', views.create_location, name='create_location'),
+    path('edit_item/<int:pk>', views.edit_item, name='edit_item'),
+    path('edit_supplier/<int:pk>', views.edit_supplier, name='edit_supplier'),
+    path('edit_location/<int:pk>', views.edit_location, name='edit_location')
 ]
--- limbs/views.py
+++ limbs/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 import random
 # Create your views here.
-from .models import Item, Manufacturer, ItemSupplier, Supplier, ItemQuantity, Location
+from .models import Item, Manufacturer, ItemSupplier, Supplier, ItemQuantity, Location, Tag
 
 def parse_form_create_item(form_data, item_id=None):
         print(form_data)
+        print(form_data.keys())
         #make new item
         temp_item = Item(
             name=form_data["item_name"],
             part_number=form_data["part_number"],
             manufacturer=Manufacturer.objects.get(name__iexact=form_data["manufacturer_name"]) #search by name
         )
-        #TODO add stuff for tags
-
         #add id 
         if item_id:
             temp_item.id = item_id
@@ -22,12 +21,15 @@ def parse_form_create_item(form_data, item_id=None):
 
         #for supplier and location table determine max id...
         supplier_max_id = -1 
-        location_max_id = -1 
+        location_max_id = -1
+        tag_max_id = -1 
         for key in form_data.keys():
             if "supplier_name_" in key:
                 supplier_max_id = max(supplier_max_id, int(key[-1]))
             if "location_name_" in key:
                 location_max_id = max(location_max_id, int(key[-1]))
+            if "tag_name_" in key:
+                tag_max_id = max(tag_max_id, int(key[-1]))
 
         #populating the through fields for suppliers
         for i in range(1, supplier_max_id+1):
@@ -46,6 +48,7 @@ def parse_form_create_item(form_data, item_id=None):
                 link = form_data[key_c],
                 cost = "{:.2f}".format(float(form_data[key_d])),
             )
+            print(item_sup)
             item_sup.save()
 
         #populating the through fields for locations
@@ -60,25 +63,38 @@ def parse_form_create_item(form_data, item_id=None):
                 location = Location.objects.get(name__iexact=form_data[key_e]),
                 quantity = form_data[key_f],
             )
+            print(item_quant)
             item_quant.save()
 
+        for i in range(1, tag_max_id+1):
+            key_g = "tag_name_" + str(i)
+
+            if key_g not in form_data: continue
+
+            limbs_tag = Tag.objects.get(name__iexact=form_data[key_g])
+
+            temp_item.tags.add(limbs_tag)        
 
 def item_table(request, search):
     item_list = Item.objects
+    print("Hello")
+    print(item_list)
     return render(request, 'limbs/item_table.html', {'item_list': item_list})
 
 def item_popup(request, pk):
     item = Item.objects.get(id=pk)
+    print(item)
     manufacturers = Manufacturer.objects.all
     suppliers = Supplier.objects.all 
-    locations = Location.objects.all 
+    locations = Location.objects.all
+    tags = Tag.objects.all
     return render(request, 
     'limbs/item_popup.html', 
     {"item":item,
     "manufacturers":manufacturers, 
     "suppliers":suppliers, 
-    "locations":locations})
-
+    "locations":locations,
+    "tags":tags})
 
 def edit_item(request, pk):
 
@@ -100,6 +116,8 @@ def create_item(request):
 
         form_data = request.POST
 
+        print(form_data)
+
         #make new item
         parse_form_create_item(form_data)
 
@@ -108,9 +126,127 @@ def create_item(request):
     else:
         manufacturers = Manufacturer.objects.all
         suppliers = Supplier.objects.all 
-        locations = Location.objects.all 
+        locations = Location.objects.all
+        tags = Tag.objects.all
         return render(request, 
         'limbs/create_item.html', 
         {"manufacturers":manufacturers, 
         "suppliers":suppliers, 
-        "locations":locations})
+        "locations":locations,
+        "tags":tags})
+
+# Pulls suppliers from DB to display in supplier_table.html
+def supplier_table(request):
+    supplier_list = Supplier.objects.all()
+    return render(request, 'limbs/supplier_table.html', {'supplier_list': supplier_list})
+
+def supplier_popup(request, pk):
+
+    supplier = Supplier.objects.get(id=pk)
+    print(supplier)
+    return render(request, 
+    'limbs/supplier_popup.html', {"supplier": supplier})
+
+def edit_supplier(request, pk):
+
+    if request.method == 'POST':
+        form_data = request.POST
+
+        #delete original item
+        Supplier.objects.filter(id=pk).delete()
+
+        #make new item with same id 
+        if "Edit" in form_data.keys():
+            parse_form_create_supplier(form_data, supplier_id=pk)
+
+    #redirect to home page
+    return HttpResponseRedirect('/limbs/supplier_list')
+
+def create_supplier(request):
+
+    if request.method == 'POST':
+
+        form_data = request.POST
+
+        print(form_data)
+
+        #make new supplier
+        parse_form_create_supplier(form_data)
+
+        #redirect to home page
+        return HttpResponseRedirect('/limbs/supplier_list')
+    else:
+        return render(request, 
+        'limbs/create_supplier.html')
+
+def parse_form_create_supplier(form_data, supplier_id=None):
+
+        temp_supplier = Supplier(
+            name=form_data["supplier_name"],
+            link=form_data["supplier_link"],
+        )
+
+        if supplier_id:
+            temp_supplier.id = supplier_id
+
+        #save the item
+        temp_supplier.save()
+
+def location_table(request):
+    location_list = Location.objects.all()
+    return render(request, 'limbs/location_table.html', {'location_list':location_list})
+
+def location_popup(request, pk):
+
+    location = Location.objects.get(id=pk)
+    locations = Locations.objects.all
+    return render(request, 
+    'limbs/location_popup.html', {
+        'location': location, "locations": locations
+    })
+
+def create_location(request):
+
+    if request.method == 'POST':
+
+        form_data = request.POST
+
+        print(form_data)
+
+        #make new supplier
+        parse_form_create_location(form_data)
+
+        #redirect to home page
+        return HttpResponseRedirect('/limbs/location_list')
+    else:
+        locations = Location.objects.all
+        return render(request, 
+        'limbs/create_location.html', {"locations":locations})
+
+def parse_form_create_location(form_data, location_id=None):
+
+    temp_location = Location(
+        name=form_data["location_name"],
+        # parent=form_data["location_parent"],
+    )
+
+    if location_id:
+        temp_location.id = location_id
+
+    #save the item
+    temp_location.save()
+
+def edit_location(request, pk):
+
+    if request.method == 'POST':
+        form_data = request.POST
+
+        #delete original item
+        Location.objects.filter(id=pk).delete()
+
+        #make new item with same id 
+        if "Edit" in form_data.keys():
+            parse_form_create_location(form_data, location_id=pk)
+
+    #redirect to home page
+    return HttpResponseRedirect('/limbs/location_list')
